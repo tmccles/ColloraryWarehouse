9dc8b0d0c865653c2d91eb5c304fd42223e4d968
tmccles <tmccles@wgu.edu>
2026-04-26 00:45:56 -0400

    updating invoice queue

--- InvoiceQueue.py
+++ InvoiceQueue.py
@@ -1,10 +1,37 @@
 #Array- base queue to hold all incoming invoices
 class InvoiceQueue:
-    def __init__(self, max_length= -1):
+    def __init__(self, maxLength= -1):
         self.invoiceList= [0]
         self.frontIndex= 0
         self.queueLength= 0
-        self.max_length= max_length
+        self.maxLength= maxLength
 
     #return the length of the queue
+    def getQueueLength(self):
+        return self.queueLength
     
+    #Return the maximum length of the invoice queue
+    def getQueueMaxLength(self):
+        return self.maxLength
+    
+    #Push a created invoice into the queue
+    def enqueueInvoice(self, invoice):
+        if self.queueLength== self.maxLength:
+            return False #This mean the queue is full
+        #reallocate the size of InvoiceQueue
+        if self.queueLength== len(self.invoiceList):
+            self.resize()
+            #Push invoice into back of queue
+            invoiceIndex= (self.frontIndex- self.queueLength) % len(self.invoiceList)
+            self.invoiceList[invoiceIndex]= invoice
+            self.queueLength += 1
+            return True
+    
+    #Remove an invoice from the queue
+    def dequeueInvoice(self):
+        #Pop invoice from the front of the queue
+        frontInvoice= self.invoiceList[self.frontIndex]
+        #Decrease the frontIndex and queueLength by 1
+        self.frontIndex= (self.frontIndex + 1) % len(self.invoiceList)
+        #Return invoice being removed
+        return frontIndex
